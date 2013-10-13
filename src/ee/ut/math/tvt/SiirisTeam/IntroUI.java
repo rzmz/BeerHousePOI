ee64f5561a73a3fe4cff76f0d587eaf75b1cb055
Lauri <lauri.uusmaa@gmail.com>
2013-10-13 22:24:12 +0300

    logo paigutus

--- src/ee/ut/math/tvt/SiirisTeam/IntroUI.java
+++ src/ee/ut/math/tvt/SiirisTeam/IntroUI.java
@@ -1,5 +1,6 @@
 package ee.ut.math.tvt.SiirisTeam;
 
+import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.io.FileInputStream;
@@ -7,10 +8,13 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
+
+import ee.ut.math.tvt.SiirisTeam.utils.ImagePanel;
 
 public class IntroUI {
 	
@@ -31,16 +35,19 @@ public class IntroUI {
 		// exit the program when window is closed by the user
 		raam.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
-		raam.setSize(500, 200);
+		raam.setSize(500, 400);
 		raam.setLocation(800, 200);
-		raam.setLayout(new GridLayout(6,1));
+		raam.setLayout(new GridLayout());
+		
+		
+		JPanel mainPanel = new JPanel(new GridLayout(7,1));
 		
 		JPanel teamNamePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		JLabel teamName = new JLabel("Team name: ");
 		JLabel prooviNimi = new JLabel(application.getProperty("team.name"));
 		teamNamePanel.add(teamName);
 		teamNamePanel.add(prooviNimi);
-		raam.add(teamNamePanel);
+		mainPanel.add(teamNamePanel);
 
 		
 		JPanel teamLeaderPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
@@ -48,14 +55,14 @@ public class IntroUI {
 		JLabel prooviNimi2 = new JLabel(application.getProperty("team.leader"));
 		teamLeaderPanel.add(teamLeader);
 		teamLeaderPanel.add(prooviNimi2);
-		raam.add(teamLeaderPanel);
+		mainPanel.add(teamLeaderPanel);
 		
 		JPanel emailPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		JLabel emailLabel = new JLabel("Team leader email:");
 		JLabel proovimeil = new JLabel(application.getProperty("team.leader_email"));
 		emailPanel.add(emailLabel);
 		emailPanel.add(proovimeil);
-		raam.add(emailPanel);
+		mainPanel.add(emailPanel);
 		
 		JPanel membersPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		JLabel membersLabel = new JLabel("Team members:");
@@ -66,22 +73,25 @@ public class IntroUI {
 		membersPanel.add(prooviliikmed0);
 		membersPanel.add(prooviliikmed1);
 		membersPanel.add(prooviliikmed2);
-		raam.add(membersPanel);
+		mainPanel.add(membersPanel);
 
-		JPanel logoPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JPanel logoPanel = new JPanel(new GridLayout(2,1));
 		JLabel logoLabel = new JLabel("Team logo:");
-		JLabel proovilogo = new JLabel(application.getProperty("team.logo"));
+		ImagePanel logo = new ImagePanel(application.getProperty("team.logo"));
 		logoPanel.add(logoLabel);
-		logoPanel.add(proovilogo);
-		raam.add(logoPanel);
+		logoPanel.add(logo);
+		mainPanel.add(logoPanel);		
 		
 		JPanel versionPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		JLabel versionLabel = new JLabel("Version number:");
 		JLabel proovivers = new JLabel(version.getProperty("build.number"));
 		versionPanel.add(versionLabel);
 		versionPanel.add(proovivers);
-		raam.add(versionPanel);
+		mainPanel.add(versionPanel);
 		
+		raam.add(mainPanel);
+		
+		raam.pack();
 		raam.setVisible(true);
 	}
 }
