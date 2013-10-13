2b00d549f748c9596f30af67d984b6eb0268970a
Rasmus Piho <rasmus.piho@gmail.com>
2013-10-13 14:53:42 +0300

    Added logging library and configuration

--- src/ee/ut/math/tvt/SiirisTeam/Intro.java
+++ src/ee/ut/math/tvt/SiirisTeam/Intro.java
@@ -1,10 +1,13 @@
 package ee.ut.math.tvt.SiirisTeam;
 
+import org.apache.log4j.Logger;
+
 public class Intro {
 
+	private static final Logger log = Logger.getLogger(Intro.class);
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		log.info("Intro main method invoked");
 	}
-
+	
 }
