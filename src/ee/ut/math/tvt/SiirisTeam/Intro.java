c2b3bc86f989ce46bbc1281652d262f5bc144bbc
Rasmus Piho <rasmus.piho@gmail.com>
2013-10-13 19:39:04 +0300

    Testing the logo image

--- src/ee/ut/math/tvt/SiirisTeam/Intro.java
+++ src/ee/ut/math/tvt/SiirisTeam/Intro.java
@@ -1,11 +1,17 @@
 package ee.ut.math.tvt.SiirisTeam;
 
+import java.awt.GridLayout;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 import org.apache.log4j.Logger;
+
+import ee.ut.math.tvt.SiirisTeam.utils.ImagePanel;
 
 public class Intro {
 
@@ -19,6 +25,22 @@ public class Intro {
 		
 		IntroUI ui = new IntroUI();
 		ui.displayTeamInfo();
+		
+		testImage();
+		
+	}
+	
+	public static void testImage(){
+		JFrame frame = new JFrame("Should be logo");
+		
+		// exit the program when window is closed by the user
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		
+		frame.setSize(640, 480);
+		frame.setLocation(900, 300);
+		ImagePanel logoPanel = new ImagePanel("assets/logo.jpg");
+		frame.add(logoPanel);
+		frame.setVisible(true);
 	}
 	
 	public static void updateVersion(){
