1374d1da3d446c01d589d6579d68dc21a2a8fae7
Rasmus Piho <rasmus.piho@gmail.com>
2013-10-13 15:31:19 +0300

    Updated version.properties, added initial logic to read version properties

--- src/ee/ut/math/tvt/SiirisTeam/Intro.java
+++ src/ee/ut/math/tvt/SiirisTeam/Intro.java
@@ -1,5 +1,8 @@
 package ee.ut.math.tvt.SiirisTeam;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import org.apache.log4j.Logger;
 
 public class Intro {
@@ -7,7 +10,24 @@ public class Intro {
 	private static final Logger log = Logger.getLogger(Intro.class);
 
 	public static void main(String[] args) {
+		
 		log.info("Intro main method invoked");
+		
+		IntroUI ui = new IntroUI();
+		ui.displayTeamInfo();
+		
+	}
+	
+	public static void updateVersion(){
+		Properties prop = new Properties();
+		try {
+			prop.load(new FileInputStream("version.properties"));
+			System.out.println(String.format("Build revision number: %s", prop.getProperty("build.revision.number")));
+			System.out.println(String.format("Build minor number: %s", prop.getProperty("build.minor.number")));
+			System.out.println(String.format("Build major number: %s", prop.getProperty("build.major.number")));
+		} catch(Exception e){
+			log.error("Error reading properties file version.properties", e);
+		}
 	}
 	
 }
