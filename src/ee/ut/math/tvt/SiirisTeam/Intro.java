642549a97a849182a55f276c122c85e33936b5dc
Rasmus Piho <rasmus.piho@gmail.com>
2013-10-13 16:32:22 +0300

    updateVersion method in main class

--- src/ee/ut/math/tvt/SiirisTeam/Intro.java
+++ src/ee/ut/math/tvt/SiirisTeam/Intro.java
@@ -1,12 +1,16 @@
 package ee.ut.math.tvt.SiirisTeam;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
 
 public class Intro {
 
+	private final static String versionPropertiesFile = "config/version.properties";
+	
 	private static final Logger log = Logger.getLogger(Intro.class);
 
 	public static void main(String[] args) {
@@ -15,17 +19,27 @@ public class Intro {
 		
 		IntroUI ui = new IntroUI();
 		ui.displayTeamInfo();
-		
 	}
 	
 	public static void updateVersion(){
 		Properties prop = new Properties();
 		try {
-			prop.load(new FileInputStream("version.properties"));
-			System.out.println(String.format("Build revision number: %s", prop.getProperty("build.revision.number")));
-			System.out.println(String.format("Build minor number: %s", prop.getProperty("build.minor.number")));
-			System.out.println(String.format("Build major number: %s", prop.getProperty("build.major.number")));
-		} catch(Exception e){
+			prop.load(new FileInputStream(Intro.versionPropertiesFile));
+			String revisionNumber = prop.getProperty("build.revision.number");
+			String minorNumber = prop.getProperty("build.minor.number");
+			String majorNumber = prop.getProperty("build.major.number");
+
+			try{
+				Integer revNr = Integer.parseInt(revisionNumber);
+				revNr++;
+				prop.setProperty("build.revision.number",  revNr.toString());
+				prop.setProperty("build.number", String.format("%s.%s.%s", majorNumber, minorNumber, revNr.toString()));
+				prop.store(new FileOutputStream(Intro.versionPropertiesFile), null);
+			} catch(NumberFormatException ex){
+				log.error("Error reading property build.revision.number", ex);
+			}
+			
+		} catch(IOException e){
 			log.error("Error reading properties file version.properties", e);
 		}
 	}
