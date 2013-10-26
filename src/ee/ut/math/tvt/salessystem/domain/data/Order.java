2e4b47c684fd182d658457df50e2261178caf2b0
siiri <siiri.saar1307@gmail.com>
2013-10-27 01:52:49 +0300

    Seekord Order.java kaasa pandud :)

---
+++ src/ee/ut/math/tvt/salessystem/domain/data/Order.java
@@ -0,0 +1,66 @@
+package ee.ut.math.tvt.salessystem.domain.data;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+public class Order implements Cloneable, DisplayableItem {
+	
+	private Long id;
+	private List<SoldItem> soldItems ;
+	private Date dateAndTime;
+	
+	public Order(Long id, List<SoldItem> soldItems){
+		this.id = id;
+		this.dateAndTime = new GregorianCalendar().getTime();
+		this.soldItems = soldItems;
+	}
+	
+	
+	public List<SoldItem> getSoldItems() {
+		return soldItems;
+	}
+
+
+	public void setSoldItems(List<SoldItem> soldItems) {
+		this.soldItems = soldItems;
+	}
+
+
+	public Date getDateAndTime() {
+		return dateAndTime;
+	}
+
+
+	public void setDateAndTime(Date dateAndTime) {
+		this.dateAndTime = dateAndTime;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public double calculateTotalSum(){
+		double sum = 0;
+		
+		for(SoldItem item: soldItems){
+			sum = sum + item.getSum();
+		}
+		return sum;
+	}
+	
+	public String returnDateAndTime(){
+		SimpleDateFormat a = new SimpleDateFormat("dd.MM.yyyy, 'kl' HH:mm:ss"); 
+		return a.format(getDateAndTime());	
+	}
+	@Override
+	public Long getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
