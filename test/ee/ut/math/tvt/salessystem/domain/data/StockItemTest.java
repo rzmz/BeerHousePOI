f0977da84bacac964caec830dcd5e174da7ffc44
Lauri <lauri.uusmaa@gmail.com>
2013-11-21 20:13:36 +0200

    added JUnit.jar, StockItemTest.java

---
+++ test/ee/ut/math/tvt/salessystem/domain/data/StockItemTest.java
@@ -0,0 +1,20 @@
+package ee.ut.math.tvt.salessystem.domain.data;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StockItemTest {
+
+	StockItem item = new StockItem(85l, "testItem", "test", 9, 23);
+
+	@Test
+	public void testClone() {
+//		assertEquals(item.clone(), new StockItem(85l, "testItem", "test", 9, 23));
+	}
+	
+	@Test
+	public void testGetColumn() {
+		assertEquals(item.getColumn(3), 23);
+	}
+}
